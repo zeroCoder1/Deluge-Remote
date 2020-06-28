651831ae3a4e79c15689f0b7098395342edacc59
Rudy Bermudez <hello@rudybermudez.io>
2020-06-28 08:39:13 -0700

    Initial Commit

---
+++ Shared/DelugeRemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DelugeRemoteApp.swift
+//  Shared
+//
+//  Created by Rudy Bermudez on 6/28/20.
+//
+
+import SwiftUI
+
+@main
+struct DelugeRemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
